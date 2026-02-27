28f62f52e5740b994fcd1a9b5d145f3eb01ecae3
Dimitar Magaranov <dimitar.magaranov1@gmail.com>
2026-02-27 16:53:05 +0200

    Initial app shell, routing, MUI, i18n

---
+++ src/pages/ProjectsPage.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+import { useI18n } from "../i18n/i18n";
+
+export default function ProjectsPage() {
+  const { t } = useI18n();
+  return <Typography variant="h4">{t.pages.projects}</Typography>;
+}
---
+++ src/pages/TransportRequestsPage.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+import { useI18n } from "../i18n/i18n";
+
+export default function TransportRequestsPage() {
+  const { t } = useI18n();
+  return <Typography variant="h4">{t.pages.transport}</Typography>;
+}
